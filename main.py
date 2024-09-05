f8143e82361c11f3553e6f61c888b83f94d49239
geropo08 <84402339+geropo08@users.noreply.github.com>
2024-09-04 21:40:59 -0300

    Fin

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-import funciones
+from funciones import *
 import leer_csv
 if __name__ == "__main__":
 
     datos = leer_csv.leer_csv('JEOPARDY_CSV.csv')
-    preguntas = funciones.preguntas_random(datos)
-    funciones.bind(funciones.juego,funciones.bind(funciones.juego,funciones.bind(funciones.juego,funciones.bind(funciones.juego,funciones.bind(funciones.juego,funciones.unit(preguntas))))))
+    preguntas = preguntas_random(datos)
+    bind(juego,bind(juego,bind(juego,bind(juego,bind(juego,unit(preguntas))))))
